57e7bc2e34901fe24e49c7b0b0ebd7acf43db7be
Duy Khoa <duykhoa12t@gmail.com>
2015-12-18 23:45:07 +0800

    Retab receipt.rb

--- receipt.rb
+++ receipt.rb
@@ -39,15 +39,15 @@ class Receipt
   # Print the receipt
   #   Something like the output format
   #
-	#   1 imported bottle of perfume: 32.19
-	#   1 bottle of perfume: 20.89
-	#   1 packet of headache pills: 9.75
-	#   1 imported box of chocolates: 11.85
-	#   Sales Taxes: 6.70
-	#   Total: 74.68
-	# May be support different format, such as :json, :csv, and :console
-	#
+  #   1 imported bottle of perfume: 32.19
+  #   1 bottle of perfume: 20.89
+  #   1 packet of headache pills: 9.75
+  #   1 imported box of chocolates: 11.85
+  #   Sales Taxes: 6.70
+  #   Total: 74.68
+  # May be support different format, such as :json, :csv, and :console
+  #
   def print(format = :console)
-		#TODO
+    #TODO
   end
 end
