ff51b5da4e3f2294a96fb1bcfb89739f8e18e543
Duy Khoa <duykhoa12t@gmail.com>
2015-12-18 23:37:07 +0800

    Improve main.rb file

--- main.rb
+++ main.rb
@@ -10,21 +10,20 @@ require "basket"
 require "receipt"
 
 # END USER CODE
-# Follow the readme pattern
+# Follow the readme pattern (README.md)
 #
 # Scenario 1:
 basket = Basket.new
 basket.load_from_file("order.csv")
 
 receipt = Receipt.new(basket)
-receipt.print 
+receipt.print
 
 # Scenario 2:
-# products << headached_pill
-# products << perfume
-# products << hamburger
+headached_pill = Product.new("headached pill")
 
 basket = Basket.new
+basket.add_item(headached_pill, quantity: 3)
 
 receipt = Receipt.new(basket)
-receipt.print 
+receipt.print
